0cc28efa21bddd37eb5c7b012d47024d910b5a57
Patrik Moledo <moledop55@gmail.com>
2023-04-22 18:23:19 +0900

    included custom hook, added Results component

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
-import Pet from "./Pet";
+import Results from './Results';
+import useBreedList from './useBreedList';
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
@@ -7,7 +8,7 @@ const SearchParams = () => {
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
   const [pets, setPets] = useState([]);
-  const breeds = []; // for now
+  const [breeds] = useBreedList(animal);
 
   useEffect(() => {
     requestPets();
@@ -23,10 +24,12 @@ const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <form onSubmit={e => {
-        e.preventDefault();
-        requestPets();
-      }}>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
@@ -68,11 +71,7 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
-      {
-        pets.map((pet) => (
-          <Pet name={pet.name} animal={pet.animal} breed={pet.breed} key={pet.id}/>
-        ))
-      }
+      <Results pets={pets} />
     </div>
   );
 };
