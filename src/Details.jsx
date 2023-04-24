29c29a08375e12b97cad023d2006a5dfd8845130
Patrik Moledo <moledop55@gmail.com>
2023-04-24 18:16:00 +0900

    broken Modal w Portal

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,11 +1,14 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import Modal from "./Modal";
 import ErrorBoundary from "./ErrorBoundary";
+import fetchPet from "./fetchPet";
 import Carousel from "./Carousel";
-import fetchPet from "./fetchPet"
 
 const Details = () => {
   const { id } = useParams();
+  const [showModal, setShowModal] = useState("");
   const results = useQuery(["details", id], fetchPet);
 
   if (results.isLoading) {
@@ -13,7 +16,7 @@ const Details = () => {
       <div className="loading-pane">
         <h2 className="loader">🌀</h2>
       </div>
-    )
+    );
   }
 
   const pet = results.data.pets[0];
@@ -21,24 +24,31 @@ const Details = () => {
   return (
     <div className="details">
       <Carousel images={pet.images} />
-      <div>
-        <h1>{pet.name}</h1>
-        <h2>
-          {pet.animal} - {pet.breed} - {pet.city}, {pet.state}
-          <button>Adopt {pet.name}</button>
+        <div>
+          <h1>{pet.name}</h1>
+          <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
+          <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
           <p>{pet.description}</p>
-        </h2>
-      </div>
+          {showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {pet.name}?</h1>
+                <div className="buttons">
+                  <button>Yes</button>
+                  <button onClick={() => setShowModal(false)}>No</button>
+                </div>
+              </div>
+            </Modal>
+          ) : null}
+        </div>
     </div>
   );
 };
 
-function DetailsErrorBoundary(props) {
+export default function DetailsErrorBoundary(props) {
   return (
     <ErrorBoundary>
       <Details {...props} />
     </ErrorBoundary>
-  )
+  );
 }
-
-export default DetailsErrorBoundary;
